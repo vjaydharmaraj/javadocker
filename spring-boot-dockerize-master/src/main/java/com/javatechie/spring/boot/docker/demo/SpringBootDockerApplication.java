f4ec8a39abaf76b5229c404168cd825c3220bb33
Vijay dharmaraj <81738255+vjaydharmaraj@users.noreply.github.com>
2023-05-04 16:03:58 +0530

    Add files via upload

---
+++ spring-boot-dockerize-master/src/main/java/com/javatechie/spring/boot/docker/demo/SpringBootDockerApplication.java
@@ -0,0 +1,23 @@
+package com.javatechie.spring.boot.docker.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class SpringBootDockerApplication {
+
+	@GetMapping("/home")
+	public String getMessage() {
+
+		return "Hello Vijay..!!";
+	}
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(SpringBootDockerApplication.class, args);
+	}
+
+}
